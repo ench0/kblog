e2db81ec8b8d68735d38b0da7fbdbd66bd581a09
Ench0 <ensar@farend.net>
2017-10-15 14:14:52 +0100

    404 formatting

--- routes/index.js
+++ routes/index.js
@@ -19,9 +19,14 @@ router
 .get('/404', function(ctx) {
         ctx.type = 'html'
         ctx.status = 404
-           
-        ctx.body = fs.createReadStream('views/404.html')
-})
+        const body = fs.readFileSync('views/404.html', 'utf8')
+
+        const ms = Date.now() - ctx.state.start    
+
+        // ctx.body = fs.createReadStream('views/404.html')
+        ctx.body = body.replace('{ms}', ms)
+        
+    })
 
 router.use('', pages.routes());
 
