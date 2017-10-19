cc800fa42a76a20b308393726672fc729827007c
admin <admin@ICCI.local>
2017-10-19 12:50:35 +0100

    fix

--- views/analytics-sample.js
+++ views/analytics-sample.js
@@ -1,5 +1,5 @@
 // Global site tag (gtag.js) - Google Analytics
-script(async src='https://www.googletagmanager.com/gtag/js?id=UA-00000000-11')
+script(async, src='https://www.googletagmanager.com/gtag/js?id=UA-00000000-11')
 script.
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
